d546576ab5dfc4717d839a9376e4e1acf74da33c
Noah Scharrenberg <nscharrenberg@hotmail.com>
2023-09-08 11:01:08 +0200

    Add Poodle Logic

--- app/Models/Poodle.php
+++ app/Models/Poodle.php
@@ -2,26 +2,58 @@
 
 namespace App\Models;
 
+use App\Helpers\Translator;
+
 class Poodle implements ILanguage
 {
+    private static string $sound = "woefie";
 
+    /**
+     * Translate the original text to the language of a Poodle.
+     *
+     * Rules: Replaces all words in the sentence with "woefie".
+     *
+     * @param ILanguage $origin - The original Language to be translated.
+     * @param string $text - The text to be translated.
+     * @return string - The translated text.
+     */
     public static function translate(ILanguage $origin, string $text): string
     {
-        // TODO: Implement translate() method.
+        return Translator::replaceAllWordWithX($text, Poodle::$sound);
     }
 
+    /**
+     * Check if the given text is valid Poodle language.
+     *
+     * @param string $text - The text to be validated.
+     * @return bool - True if the text is valid in Poodle Language, False otherwise.
+     */
     public static function valid(string $text): bool
     {
-        // TODO: Implement valid() method.
+        return Translator::sentenceContainsOnlyWords($text, [Poodle::$sound]);
     }
 
+    /**
+     * Detect if the given text is Poodle Language.
+     *
+     * @param string $text - The text to be analysed.
+     * @return bool - True if Poodle language is detected, False otherwise.
+     */
     public static function detected(string $text): bool
     {
-        // TODO: Implement detected() method.
+        return Labrador::valid($text);
     }
 
+    /**
+     * Get a list of classes that can translate the Poodle language.
+     *
+     * @return string[] - A list of languages that can translate the Poodle language.
+     */
     public static function translateable(): array
     {
-        // TODO: Implement translateable() method.
+        return [
+          Labrador::class,
+          Parrot::class
+        ];
     }
 }
