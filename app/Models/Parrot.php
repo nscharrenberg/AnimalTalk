21b0365e5c2337a9f808bad0ed943bf6783afbe1
Noah Scharrenberg <nscharrenberg@hotmail.com>
2023-09-08 10:58:23 +0200

    Add Parrot Logic

--- app/Models/Parrot.php
+++ app/Models/Parrot.php
@@ -5,23 +5,51 @@ namespace App\Models;
 class Parrot implements ILanguage
 {
 
+    /**
+     * Translate the original text to the language of a Parrot.
+     *
+     * Rules: Start the sentence with "ik praat je na " followed by the given text.
+     *
+     * @param ILanguage $origin - The original Language to be translated.
+     * @param string $text - The text to be translated.
+     * @return string - The translated text.
+     */
     public static function translate(ILanguage $origin, string $text): string
     {
-        // TODO: Implement translate() method.
+        return "Ik praat je na {$text}";
     }
 
+    /**
+     * Check if the given text is valid Parrot language.
+     *
+     * @param string $text - The text to be validated.
+     * @return bool - Always false, as we can not translate Parrot Language to any other language.
+     */
     public static function valid(string $text): bool
     {
-        // TODO: Implement valid() method.
+        return false;
     }
 
+    /**
+     * Detect if the given text is Parrot Language.
+     *
+     * @param string $text - The text to be analysed.
+     * @return bool - Always False, as we can not translate Parrot Language to any other language.
+     */
     public static function detected(string $text): bool
     {
-        // TODO: Implement detected() method.
+        return false;
     }
 
+    /**
+     * Get a list of classes that can translate the Parrot language.
+     *
+     * @return string[] - A list of languages that can translate the Parrot language.
+     */
     public static function translateable(): array
     {
-        // TODO: Implement translateable() method.
+        return [
+
+        ];
     }
 }
